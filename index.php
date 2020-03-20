9651c563c51ff58a08b6fab5fa9f481552d5ecca
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-03-20 14:10:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -24,18 +24,17 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
     </li>
     <li>
     <a href="#" onclick="signOut();">Sign out</a>
-      <script>
+      </script>
+    </li>
+  </ul>
+</nav>
+<script>
         function signOut() {
           var auth2 = gapi.auth2.getAuthInstance();
           auth2.signOut().then(function () {
             console.log('User signed out.');
           });
         }
-      </script>
-    </li>
-  </ul>
-</nav>
-
   <div id="container">
       <?php
       switch ($module) {
